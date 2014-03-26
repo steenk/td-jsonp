88a7dec154f1d1053c02e25d97c7549e2408a2af
Steen Klingberg <steen@sk-ab.se>
2014-03-26 08:22:41 +0100

    version 0.2.0

--- jsonp.js
+++ jsonp.js
@@ -1,14 +1,25 @@
+/* kitsi jsonp 0.2.0 */
 define(function () {
 	return  function (url, callback) {
+		var res, cb, r = url.match(/[?&]callback=(.+)/);
+		var id = 'script' + (Math.random()*67108864|0).toString(16);
+		if (r) {
+			cb = r[1];
+		} else {
+			cb = id;
+		}
+		window[cb] = function (data) {
+			res = JSON.parse(data);
+		}
   		var script = document.createElement('script');
     	script.type = 'text/javascript';
     	script.src = url;
     	script.async = true;
-    	var id = 'script' + (Math.random()*67108864|0).toString(16);
     	script.id = id;
     	script.onreadystatechange = script.onload = function() {
         	script.parentNode.removeChild(document.getElementById(id));
-        	callback && callback();
+			delete window[cb];
+        	callback && callback(res);
     	};
     	(document.body || document.head).appendChild(script);
 	}
